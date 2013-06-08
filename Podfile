e64577ddd4cbe2205fd5891bb1c5786641728c2f
Osamu Noguchi <atrac613@gmail.com>
2013-06-08 22:02:42 +0900

    add ocmock via cocoapods.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios
+
+target :SenTestingKitTests do
+    pod 'OCMock'
+end
---
+++ SenTestingKitTests/OCMockSampleTests.h
@@ -0,0 +1,13 @@
+//
+//  OCMockSampleTests.h
+//  SenTestingKit
+//
+//  Created by Osamu Noguchi on 6/8/13.
+//  Copyright (c) 2013 Osamu Noguchi. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OCMockSampleTests : SenTestCase
+
+@end
